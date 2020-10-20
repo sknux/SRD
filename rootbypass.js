eba56069fd2da0abdfbbec30e6fc557aa765c4d7
n3k00n3 <n3k00n3@protonmail.com>
2020-10-20 19:38:35 -0300

    code refactor

---
+++ rootbypass.js
@@ -0,0 +1,28 @@
+'use strict'
+
+if (Java.available){
+
+  Java.perform(function(){
+
+    try{
+
+    var target = asd
+
+    target.implementation = function () {
+    console.log('');
+    console.log('[+] Bypassed Root Detection, Enjoy!');
+
+    return false;
+    };
+
+    }
+    catch(error){
+      console.log("[-] An exception occured");
+      console.log(String(error.stack));
+    }
+  });
+}
+else{
+
+console.log("[-] Java is not available");
+}
